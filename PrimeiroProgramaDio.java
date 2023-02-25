98ed1a1def28f0e06799576bc7839e0c75905bcb
wesley hallan da silva cunha <wesleyhallan.cunha@gmail.com>
2023-02-25 12:05:47 -0300

    Add files via upload

---
+++ PrimeiroProgramaDio.java
@@ -0,0 +1,6 @@
+package com.dio.bootcamp.primeiroprogramadio;
+public class PrimeiroProgramaDio {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
